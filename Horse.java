c16b8b01d794f55b2cc6adc59155a1c11736464e
Dahiver Guerra <95004742+Dreamzur@users.noreply.github.com>
2022-10-07 23:22:08 +0000

    finished

--- Horse.java
+++ Horse.java
@@ -1,1 +1,27 @@
 //Create the Horse class here.
+public class Horse {
+
+  private String horseName;
+  private double horseWeight;
+  private boolean horseTame;
+
+  public Horse (String horseName, double horseWeight, boolean horseTame){
+
+    this.horseName = horseName;
+    this.horseWeight = horseWeight;
+    this.horseTame = horseTame;
+  }
+
+  public String getName(){
+    return this.horseName;
+  }
+
+  public double getWeight(){
+    return this.horseWeight;
+  }
+
+  public boolean getTame(){
+    return this.horseTame;
+  }
+  
+}
--- Stable.java
+++ Stable.java
@@ -1,1 +1,9 @@
+import java.util.ArrayList;
+
 //Create the Stable class here.
+public class Stable {
+
+  String address;
+  ArrayList<Horse> horse = new ArrayList<>();
+
+}
